c7b3a5072f6ff030ccc56ba8bb6673a4e164a7c9
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2025-03-11 17:09:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,4 @@
-resource "google_storage_bucket" "infra-bucket" {
-  name          = "bkt-demo-000"
-  location      = "us-central1"
-  project       = "content-gen-418510"
-  force_destroy = true
-  public_access_prevention = "enforced"
-}
-
-resource "google_storage_bucket" "infra-bucket2" { # Fixed name
-  name          = "bkt-demo-001"  # Changed name to be unique
-  location      = "us-central1"
-  project       = "tt-dev-001"
-  force_destroy = true
-  public_access_prevention = "enforced"
+resource "google_compute_project_metadata_item" "test" {
+  key   = "test-metadata"
+  value = "Hello from Terraform"
 }
