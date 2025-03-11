3e529e22d3f24cecca027831cdd5f43b6092e16c
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2025-03-11 17:08:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 resource "google_storage_bucket" "infra-bucket" {
   name          = "bkt-demo-000"
   location      = "us-central1"
-  project = "content-gen-418510"
+  project       = "content-gen-418510"
   force_destroy = true
   public_access_prevention = "enforced"
 }
 
-resource "google_storage_bucket" "infra--bucket2" {
-  name          = "content-gen-418510"
+resource "google_storage_bucket" "infra-bucket2" { # Fixed name
+  name          = "bkt-demo-001"  # Changed name to be unique
   location      = "us-central1"
-  project = "tt-dev-001"
+  project       = "tt-dev-001"
   force_destroy = true
   public_access_prevention = "enforced"
 }
