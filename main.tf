b6781395c2ff50797120056e832b5b1600c589e9
Saksham Awasthi <74664860+Cipher-08@users.noreply.github.com>
2025-03-11 17:17:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,7 @@
-resource "google_compute_project_metadata_item" "test" {
-  key   = "test-metadata"
-  value = "Hello from Terraform"
+resource "random_id" "test_id" {
+  byte_length = 8
+}
+
+output "random_string" {
+  value = random_id.test_id.hex
 }
